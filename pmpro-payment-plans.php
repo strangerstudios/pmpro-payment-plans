916a856f739f13221f4f98fc5524abc62c896261
Jarryd Long <JarrydLong@users.noreply.github.com>
2022-03-14 11:52:19 +0200

    Checks in place to fix possible PHP Warning

--- pmpro-payment-plans.php
+++ pmpro-payment-plans.php
@@ -247,7 +247,7 @@ function pmpropp_return_payment_plans( $level_id, $plan_id = '' ) {
 		 * @param bool $include_level_price Should the levels default pricing be automatically included at checkout.
 		 * @param int $level_id The level ID value of current checkout/level in question.
 		 */
-		if ( apply_filters( 'pmpropp_include_level_pricing_option_at_checkout', true, $level_id ) && ( is_page( $pmpro_pages['checkout'] ) || wp_doing_ajax() ) ) {
+		if ( apply_filters( 'pmpropp_include_level_pricing_option_at_checkout', true, $level_id ) && ( is_page( $pmpro_pages['checkout'] ) || wp_doing_ajax() ) && is_array( $payment_plans ) ) {
 			$level = pmpro_getLevel( $level_id );
 			$level->status = 'active';
 			$level->default = 'yes'; //Default to yes, as it can be adjusted by "real" plans later on.
